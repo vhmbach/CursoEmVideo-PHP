3df29558a55d836b325ece23f8562f22fa9a6eab
Vitor Bach <vitorhugo.bach@gmail.com>
2022-10-22 21:47:00 -0300

    CursoEmVideo-PHP

---
+++ Aula03/OlaMundo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+    <!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Primeiro Exemplo PHP</title>
+    </head>
+    <body>
+        <h1>Testando PHP</h1>
+        <?php
+            echo "Olá Mundo";
+        ?>
+    </body>
+    </html>
---
+++ Aula04/echo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+       <meta charset="UTF-8">
+       <style></style>
+       <title></title>
+    </head>
+    <body>
+    <?php
+    $n = 4;
+    $nome = "Vitor";
+    echo "$nome";
+
+    ?>
+    </body>
+</html>
